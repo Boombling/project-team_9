a3f9a1062279b72757cf4670bbe7ffe170c6a7a6
Dmytro Filius <mindertan@gmail.com>
2021-05-10 01:18:34 +0300

    робота над запитом до апі

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,26 @@
-import ConcertApiService from './services/services';
-import concertCard from '../templates/card-list.hbs';
+import EventsApiService from './services/services';
+import eventsCard from '../templates/card-list.hbs';
 
 const cardContainer = document.querySelector('.gallery');
-const concertApiService = new ConcertApiService();
+const eventsApiService = new EventsApiService();
 // console.log(concertApiService);
 
-// fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=MMQ2M3AOTcNvFmVoIxNGUGotXqF5t9MP&size=20')
-//     .then(response => response.json())
-//     .then(console.log);
 
 function onSerch(e) {
     try {
-        const response = concertApiService.fetchConcert();
-        const render = renderConsertList(response);
+        const response = eventsApiService.fetchEvents();
+        const render = renderEventsList(response);
         return render;
     }
     catch {
         onFetchError();
     }
 }
-function renderConsertList(events) {
-   cardContainer.insertAdjacentHTML('beforeend', concertCard(events));
+function renderEventsList(embedded) {
+   cardContainer.insertAdjacentHTML('beforeend', eventsCard(embedded));
 }
 function onFetchError(error) {
-    console.log('This country not found')
+    console.log('ERROR')
 }
+
 onSerch()
--- src/js/services/services.js
+++ src/js/services/services.js
@@ -1,47 +1,36 @@
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json'
 const API_KEY = 'MMQ2M3AOTcNvFmVoIxNGUGotXqF5t9MP';
 // console.log(BASE_URL);
-export default class ConcertApiService {
+export default class EventsApiService {
     constructor() {
-        // this.page = 1;
-        // this.size = 20;
-        // this.keyword = '';
+        this.page = 1;
+        this.size = 20;
+        this.keyword = 'us';
     }
 
-    fetchConcert() {
-        // const url = `${BASE_URL}?apikey=${API_KEY}&page=${this.page}&size=${thsi.size}&keyword=${this.keyword}`
-        // const url = `https://app.ticketmaster.com/discovery/v2/events.json?attractionId=K8vZ917Gku7&countryCode=CA&apikey=MMQ2M3AOTcNvFmVoIxNGUGotXqF5t9MP`;
-        const url = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=MMQ2M3AOTcNvFmVoIxNGUGotXqF5t9MP&size=20';
+    fetchEvents() {
+        console.log(this);
+        const url = `${BASE_URL}?apikey=${API_KEY}&size=${this.size}&page=${this.page}&keyword=${this.keyword}&countryCode=us`;
         return fetch(url)
             .then(respons => respons.json())
+            .then(({ events }) => {
+                this.incrementPage();
+                return events;
+            })
             .catch(error => console.log(error))
-    }    
+    }
+
+    incrementPage(){
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 1;
+    }
+    // get query(){
+    //     return this.keyword;
+    // }
+
+    // set query(newQuery){
+    //     this.keyword = newQuery;
+    // }
 }
-
-
-//  async fetchConcert() {
-//         // const url = `${BASE_URL}?apikey=${API_KEY}&page=${this.page}&size=${thsi.size}&keyword=${this.keyword}`
-//         // const url = `https://app.ticketmaster.com/discovery/v2/events.json?attractionId=K8vZ917Gku7&countryCode=CA&apikey=MMQ2M3AOTcNvFmVoIxNGUGotXqF5t9MP`;
-//            const url = await fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=MMQ2M3AOTcNvFmVoIxNGUGotXqF5t9MP&size=20')
-//         if (!url.ok) {
-//             throw url;
-//         }
-//         const result = await url.json();
-//         return result;
-//     }   
-// export default class NewsfetchImg {
-//   constructor() {
-//     const nameSearchImg = '';
-//     this.page = 1;
-//   }
-//   async fetchImg() {
-//     const rawResult = await fetch(
-//       ${BASE_URL}/api/?image_type=photo&orientation=horizontal&q=${this.nameSearchImg}&page=${this.page}&per_page=12&key=${API_KEY}
-//     );
-//     if (!rawResult.ok) {
-//       throw rawResult;
-//     }
-//     const result = await rawResult.json();
-//     this.page += 1;
-//     return result;
-//   }}
