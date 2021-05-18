01c35df004e8ecb17516fb37e78f400d36f4c0ef
Volodymyr Viter <mad.tvido@gmail.com>
2021-05-18 17:24:04 +0300

    Add animation on Logo
    Добавляє анімацію при ховері на Logo

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './styles.css';
 import './js/main.js';
 import './js/components/preloader';
 // import './js/services/choose-country';
+// import './js/components/animationbg';
+import './js/components/animcards';
