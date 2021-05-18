dfbb61353c2092d21d5bd46afdfa0926d1e098e2
Volodymyr Viter <mad.tvido@gmail.com>
2021-05-18 17:29:41 +0300

    Add background animation
    Добавляє анімацію фону.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import './js/components/preloader';
 // import './js/services/choose-country';
 // import './js/components/animationbg';
 import './js/components/animcards';
+import './js/components/animationbg';
