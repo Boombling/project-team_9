3f4101d29092dff8556f27822535fae8d8cc5e42
Dmytro Filius <mindertan@gmail.com>
2021-05-20 21:59:26 +0300

    Update img-render-list.js

--- src/js/utils/img-render-list.js
+++ src/js/utils/img-render-list.js
@@ -3,7 +3,7 @@ export default function createsDownloadList(lists){
     const listOfRendr = [];
     lists.map(list =>{
       const listsUrl = list.images.filter(img =>
-               String(img.url).includes('RETINA_PORTRAIT_3_2'));
+               String(img.url).includes('ARTIST_PAGE_3_2'));
 
              const newRenderEventList ={
                 imag: listsUrl[0].url,
